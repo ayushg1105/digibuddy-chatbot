f1442d3b324cce97d42559d88316079e1487fcac
ayushg1105 <aygarg11@outlook.com>
2025-05-01 15:19:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     window.location.href = 'main.html'; // Redirect to main page after login
   });
   
+document.getElementById('btn-tutorials').addEventListener('click', function () {
+  window.location.href = 'tutorials.html';
+});
+
+document.getElementById('btn-feedback').addEventListener('click', function () {
+  window.location.href = 'feedback.html';
+});
